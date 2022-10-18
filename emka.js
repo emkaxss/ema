bcd9785e6f47cfd700ef5588e30397b807f06ff7
emkaxss <em1llbek0vv@gmail.com>
2022-10-18 09:27:44 +0600

    uhyuyb

---
+++ emka.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+// Задание-1
+//  let n = prompt('write the number')
+//  console.log( n % 5);
+
+
+// Задание -2
+// let x = prompt("write first number");
+// let y = prompt("write your second number");
+// function min(x, y) {
+//     if (x < y) {
+//       return x;
+//     } 
+//     else {
+//       return y;
+//     }
+//   }
+//   console.log(min(x,y));
+
+
+
+
+// Задание-3
+// let n = prompt("write the num");
+// let sum = 0;
+
+// while (n > 0) {
+//     sum += n % 10
+//     n = Math.floor(n/10)
+// }
+// console.log(sum)
